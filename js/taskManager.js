f1c2429b015e9b858340f0cafb605632fc3bcaf4
getuengda <getuengda@gmail.com>
2021-08-19 13:17:23 -0400

    Update taskManager.js

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,1 +1,17 @@
-
+class TaskManager {
+  constructor(currentId = 0){
+    this.tasks = []
+    this.currentId = currentId;
+}
+  addTask () {
+    id: 1,
+    name: 'Take out the trash',
+    description: 'Take out the trash to the front of the house',
+    assignedTo: 'Nick',
+    dueDate: '2020-09-20',
+    status: 'TODO'
+    for (id = 0; id < currentId.length; id++){
+    this.tasks.push(this.currentId, name, description, assignedTo, dueDate, status = 'TODO')  )
+    }
+}
+};
