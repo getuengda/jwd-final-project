04962982f981c8919aac0bdb6b31c2de1c064e18
getuengda <getuengda@gmail.com>
2021-08-19 14:38:59 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,6 @@
 const newTask = new TaskManager('task1');
 console.log(addTask(tasks));
+
+newTask.addEventListener("submit", name, description, assignedTo, dueDate, status);
+event.preventDefault();
+
