947ed9642503a61af4108c09f70b18fe47bee352
getuengda <getuengda@gmail.com>
2021-08-23 13:34:15 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -34,3 +34,39 @@ function validFormFieldInput(data){
     return data !== null && data !== '';
 }
 
+function validFormFieldInput(data){
+    const newTaskNameInput = document.querySelector('#Task');
+    const taskname = newTaskNameInput.value;
+    console.log("name:  "+ taskname);
+    
+    const newDescriptionInput = document.querySelector('#Description');
+    const description = newDescriptionInput.value;
+    console.log("name:  "+description);
+    
+    const newDueDateInput = document.querySelector('#Due');
+    const duedate = newDueDateInput.value;
+    console.log("name:  "+newDueDateInput);
+    
+    const newAssignementInput = document.querySelector('#Assign');
+    const assignement = newAssignementInput.value;
+    console.log("name:  "+assignement);
+    
+    const newStatusInput = document.querySelector('#Status');
+    const status = newStatusInput.value;
+    console.log("name:  "+status);
+
+    if (!data){
+    const validInput = document.querySelector('#alert');
+    const status = validInput.value;
+    console.log("Warning:  "+validInput);
+
+    }
+}
+function validFormFieldInput() {
+    var x = document.getElementById("alert");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
